c23d9d2173ac3bd5f286d5b1e38404e54d552288
st50113eric <a0972371071@gmail.com>
2018-09-26 13:07:37 +0800

    test

--- 2018/9/GradientDescent.py
+++ 2018/9/GradientDescent.py
@@ -1,10 +1,22 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-train = np.loadtxt('ResearchRandomizer.csv',delimiter=',',dtype='int',skiprows=1)
+# https://www.mockaroo.com/
+train = np.loadtxt('./2018/9/ResearchRandomizer.csv',delimiter=',',dtype='int',skiprows=1)
 df=pd.DataFrame(train)
 print(df)
 train_x = train[:,0]
 train_y = train[:,1]
 plt.scatter(train_x, train_y)
+plt.subplot(2,1,1)
+
+#標準化
+mu = train_x.mean()
+sigma = train_x.std()
+def standardize(x):
+    return (x-mu) / sigma
+train_z = standardize(train_x)
+print(train_z)
+plt.scatter(train_z, train_y)
+plt.subplot(2,1,2)
 plt.show()
