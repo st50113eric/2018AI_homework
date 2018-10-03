ed5d80162024fdb05ad3f00d2d3ef6f0be918b1c
st50113eric <a0972371071@gmail.com>
2018-10-03 17:14:48 +0800

    HW

--- 2018/9/decisionTreeAndRandomForest.py
+++ 2018/9/decisionTreeAndRandomForest.py
@@ -1,103 +1,28 @@
 import numpy as np 
 import pandas as pd 
-from sklearn.metrics import confusion_matrix 
-from sklearn.cross_validation import train_test_split 
-from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-from sklearn import tree
-  
-# Function importing Dataset 
-def importdata(): 
-    balance_data = pd.read_csv( './2018/9/Skyserver_SQL2_27_2018 6_51_39 PM.csv') 
-    print ("Dataset rows: ", len(balance_data)) 
-    print ("Dataset shape: ", balance_data.shape) 
-      
-    # Printing the dataset obseravtions 
-    print ("Dataset: ",balance_data.head()) 
-    return balance_data 
-  
-# Function to split the dataset 
-def splitdataset(balance_data): 
-  
-    # Seperating the target variable
-    
-    X = balance_data.values[:, 1:5] 
-    Y = balance_data.values[:, 0] 
-    
-
-    # Spliting the dataset into train and test 
-    X_train, X_test, y_train, y_test = train_test_split(  
-    X, Y, test_size = 0.3, random_state = 100) 
-  
-    return X, Y, X_train, X_test, y_train, y_test 
-      
-# Function to perform training with giniIndex. 
-def train_using_gini(X_train, X_test, y_train): 
-  
-    # Creating the classifier object 
-    clf_gini = DecisionTreeClassifier(criterion = "gini", 
-            random_state = 100,max_depth=3, min_samples_leaf=5) 
-  
-    # Performing training 
-    clf_gini.fit(X_train, y_train) 
-    return clf_gini 
-      
-# Function to perform training with entropy. 
-def tarin_using_entropy(X_train, X_test, y_train): 
-  
-    # Decision tree with entropy 
-    clf_entropy = DecisionTreeClassifier( 
-            criterion = "entropy", random_state = 100, 
-            max_depth = 3, min_samples_leaf = 5) 
-  
-    # Performing training 
-    clf_entropy.fit(X_train, y_train) 
-    return clf_entropy 
-  
-  
-# Function to make predictions 
-def prediction(X_test, clf_object): 
-  
-    # Predicton on test with giniIndex 
-    y_pred = clf_object.predict(X_test) 
-    print("Predicted values:") 
-    print(y_pred) 
-    return y_pred 
-      
-# Function to calculate accuracy 
-def cal_accuracy(y_test, y_pred): 
-      
-    print("Confusion Matrix: ", 
-        confusion_matrix(y_test, y_pred)) 
-      
-    print ("Accuracy : ", 
-    accuracy_score(y_test,y_pred)*100) 
-      
-    print("Report : ", 
-    classification_report(y_test, y_pred)) 
-  
-# Driver code 
-def main(): 
-       
-    data = importdata() 
-    X, Y, X_train, X_test, y_train, y_test = splitdataset(data) 
-    clf_gini = train_using_gini(X_train, X_test, y_train) 
-    clf_entropy = tarin_using_entropy(X_train, X_test, y_train) 
-      
-    # Operational Phase 
-    print("Results Using Gini Index:") 
-      
-    # Prediction using gini 
-    y_pred_gini = prediction(X_test, clf_gini) 
-    cal_accuracy(y_test, y_pred_gini) 
-      
-    print("Results Using Entropy:") 
-    # Prediction using entropy 
-    y_pred_entropy = prediction(X_test, clf_entropy) 
-    cal_accuracy(y_test, y_pred_entropy) 
-      
-      
-# Calling main function 
-if __name__=="__main__": 
-    main() 
+from sklearn.model_selection import train_test_split
+spaceData = pd.read_csv( './2018/9/Skyserver_SQL2_27_2018 6_51_39 PM.csv')
+print ("Dataset Length: ", len(spaceData)) 
+print ("Dataset Shape: ", spaceData.shape) 
+print (spaceData.head(5)) # Printing the dataset obseravtions 
+x = pd.DataFrame(spaceData['data'], column=spaceData['objid','ra','dec','u','g','r','i','z'])
+y = pd.DataFrame("target_names: "+ str(spaceData["class"]))
+ 
+# processData = spaceData[['objid','ra','dec','u','g','r','i',
+#                   'z','run','rerun','camcol','field','specobjid', 'class','redshift','plate','mjd','fiberid']]
+# x = processData[['objid','ra','dec','u','g','r','i',
+#                   'r','i','z']]
+# y = processData[['class']]
+# for i in range(0, len(processData)):
+#     if (y['class'][i]) == "GALAXY":
+#         y['class'][i] = 2
+#     if (y['class'][i]) == "STAR":
+#         y['class'][i] = 1
+#     else:
+#         y['class'][i] = 0
+# print(y)
+# X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
+# tree = DecisionTreeClassifier(criterion = 'entropy', max_depth=3, random_state=0)
+# tree.fit(X_train,y_train)
